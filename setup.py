fe34f82617648ae58d827dfc02713c611317f321
Stuart Asiimwe <stuartgabriel@ymail.com>
2025-04-22 21:36:58 +0900

    Fix formatting in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='blackboxml',
     version='0.1.0',
-    author='Stuart Asiimwe'
+    author='Stuart Asiimwe',
     description='An Auto-metric logger library for Machine Learning workflows, even when you forget.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
