9763182b2c80d1a1421ca557612b71f0e01cf601
Stuart Asiimwe <stuartgabriel@ymail.com>
2025-04-27 01:28:02 +0900

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blackboxml',
-    version='0.1.0',
+    version='0.1.1',
     author='Stuart Asiimwe',
     author_email='stuartgabriel@ymail.com',
     description='An Auto-metric logger library for Machine Learning workflows, even when you forget.',
