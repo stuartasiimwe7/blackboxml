16b69b1589e0cae8f6cee672e9ef0fb5566d28df
Stuart Asiimwe <stuartgabriel@ymail.com>
2025-07-02 02:06:48 +0900

    deprecation of BlackBoxML

--- blackboxml/__init__.py
+++ blackboxml/__init__.py
@@ -1,3 +1,12 @@
+import warnings
+
+print("[DEPRECATION WARNING] The 'blackboxml' package is deprecated and will be renamed to 'traceml'. Please update your dependencies and imports to use 'traceml' instead. This package will no longer be maintained under the old name.\nTo install the new package, run: pip install traceml")
+warnings.warn(
+    "The 'blackboxml' package is deprecated and will be renamed to 'traceml'. Please install and use 'traceml' instead. To install, run: pip install traceml. See the documentation for migration instructions.",
+    DeprecationWarning,
+    stacklevel=2
+)
+
 from .autopilot import autopilot, Tracker
 
 # Patch model.fit when imported
