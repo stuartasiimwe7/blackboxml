dd4d6479ad373a877fb485524c3437fae28f8a3d
Stuart Asiimwe <stuartgabriel@ymail.com>
2025-07-02 23:21:31 +0900

    bye

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blackboxml',
-    version='0.1.1',
+    version='0.2.0',
     author='Stuart Asiimwe',
     author_email='stuartgabriel@ymail.com',
     description="Automatically logs Keras training metrics.",
